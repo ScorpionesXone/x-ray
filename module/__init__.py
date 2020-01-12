19ddd4c17697dcc936892ca698469ef498faaaed
ScorpionesXone <37965475+ScorpionesXone@users.noreply.github.com>
2020-01-12 18:04:35 +0300

    Update __init__.py

--- module/__init__.py
+++ module/__init__.py
@@ -103,74 +103,74 @@ def residual(params, windrad, path):
     return (lis[1]-model) / lis[2]
 
 
-    def residualArr(params, x, data, eps_data, windrad):
-        """Function to be minimized
+def residualArr(params, x, data, eps_data, windrad):
+    """Function to be minimized
 
-        Keyword arguments:
-        params -- Parameters involved in th fitting
-        x -- The set of values of the moments of observations in days
-        data -- Source luminosity at these moments
-        eps_data -- Observation errors
-        windrad -- wind and radius parameter for determining the model used
-        """
+    Keyword arguments:
+    params -- Parameters involved in th fitting
+    x -- The set of values of the moments of observations in days
+    data -- Source luminosity at these moments
+    eps_data -- Observation errors
+    windrad -- wind and radius parameter for determining the model used
+    """
 
-        rout = params['rout']
-        alpha = params['alpha']
-        Mdotin = params['Mdotin']
-        x0 = params['x0']
+    rout = params['rout']
+    alpha = params['alpha']
+    Mdotin = params['Mdotin']
+    x0 = params['x0']
 
+    
+    default_kwargs = dict(wind=b'no', Mdotout=Mdotout, Mx=Mx, kerr = kerr, alpha = alpha,
+            initialcond=b'quasistat', powerorder=1, opacity=b'OPAL', boundcond = b'Tirr', Thot = 1e4, 
+            Cirr = Cirr, time=35*DAY, tau=0.3*DAY, Nx=10000, gridscale=b'linear') 
+    
+    def run(**input_kwargs):
+        kwargs = default_kwargs.copy()
+        kwargs.update(input_kwargs)
+        fr = Freddi(**kwargs)
+        return fr
+    
+    
+    if windrad == 'wind and tidal':
+        frwT = run(wind=b'__Woods_Which_Shields__' , windparams=[_Xi_max, _T_iC], Mdotin = Mdotin, rout = None, Mopt = Mopt, period = period)
+        resultT  = frwT.evolve()
+
+        track = interpolate.splrep(resultT.t + x0*DAY, resultT.Mdot_in)
+        model = interpolate.splev(x, track)
+         
+    elif windrad == 'wind no tidal':
+        frw = run(rout = rout, F0=Mdotin*np.sqrt(GM*rout), wind=b'__Woods_Which_Shields__' , windparams=[_Xi_max, _T_iC])
+        result  = frw.evolve()
+
+        track = interpolate.splrep(result.t + x0*DAY, result.Mdot_in)
+        model = interpolate.splev(x, track)
+                   
+    elif windrad == 'no wind and tidal':
+        fr0T = run(Mdotin = Mdotin, rout = None, Mopt = Mopt, period = period)
+        r0T  = fr0T.evolve()
+
+        track = interpolate.splrep(r0T.t + x0*DAY, r0T.Mdot_in)
+        model = interpolate.splev(x, track)
+    elif windrad == 'no wind no tidal':
+        fr0 = run(rout = rout, F0=Mdotin*np.sqrt(GM*rout))
+        r0  = fr0.evolve()
         
-        default_kwargs = dict(wind=b'no', Mdotout=Mdotout, Mx=Mx, kerr = kerr, alpha = alpha,
-                initialcond=b'quasistat', powerorder=1, opacity=b'OPAL', boundcond = b'Tirr', Thot = 1e4, 
-                Cirr = Cirr, time=35*DAY, tau=0.3*DAY, Nx=10000, gridscale=b'linear') 
-        
-        def run(**input_kwargs):
-            kwargs = default_kwargs.copy()
-            kwargs.update(input_kwargs)
-            fr = Freddi(**kwargs)
-            return fr
-        
-        
-        if windrad == 'wind and tidal':
-            frwT = run(wind=b'__Woods_Which_Shields__' , windparams=[_Xi_max, _T_iC], Mdotin = Mdotin, rout = None, Mopt = Mopt, period = period)
-            resultT  = frwT.evolve()
-
-            track = interpolate.splrep(resultT.t + x0*DAY, resultT.Mdot_in)
-            model = interpolate.splev(x, track)
-             
-        elif windrad == 'wind no tidal':
-            frw = run(rout = rout, F0=Mdotin*np.sqrt(GM*rout), wind=b'__Woods_Which_Shields__' , windparams=[_Xi_max, _T_iC])
-            result  = frw.evolve()
-
-            track = interpolate.splrep(result.t + x0*DAY, result.Mdot_in)
-            model = interpolate.splev(x, track)
-                       
-        elif windrad == 'no wind and tidal':
-            fr0T = run(Mdotin = Mdotin, rout = None, Mopt = Mopt, period = period)
-            r0T  = fr0T.evolve()
-
-            track = interpolate.splrep(r0T.t + x0*DAY, r0T.Mdot_in)
-            model = interpolate.splev(x, track)
-        elif windrad == 'no wind no tidal':
-            fr0 = run(rout = rout, F0=Mdotin*np.sqrt(GM*rout))
-            r0  = fr0.evolve()
-            
-            track = interpolate.splrep(r0.t + x0*DAY, r0.Mdot_in)
-            model = interpolate.splev(x, track)
-        else:
-            print('Something is wrong')
-        
-        return (data-model) / eps_data
+        track = interpolate.splrep(r0.t + x0*DAY, r0.Mdot_in)
+        model = interpolate.splev(x, track)
+    else:
+        print('Something is wrong')
+    
+    return (data-model) / eps_data
 
 
-params = Parameters()
-params.add('rout', min= 1e11, value = 1.7e11, max = 2e11)
-params.add('alpha', min = 0.1, value = 0.8, max = 1.0)
-params.add('Mdotin', min = 1e18, value = 1e19, max = 1.5e19)
-params.add('x0', min= 1.0, value = 3.5, max = 5.0)
-params['x0'].vary = False
+#params = Parameters()
+#params.add('rout', min= 1e11, value = 1.7e11, max = 2e11)
+#params.add('alpha', min = 0.1, value = 0.8, max = 1.0)
+#params.add('Mdotin', min = 1e18, value = 1e19, max = 1.5e19)
+#params.add('x0', min= 1.0, value = 3.5, max = 5.0)
+#params['x0'].vary = False
 
-def fit(wind, path):
+def fit(params, wind, path):
     """Function to fit the light curve
 
     Keyword arguments:
@@ -180,7 +180,7 @@ def fit(wind, path):
     o = minimize(residual, params, args=(wind, path))
     return o
 
-def fitnp(x, data, eps_data, windrad):
+def fitnp(params, x, data, eps_data, windrad):
     """Function to fit the light curve
 
     Keyword arguments:
